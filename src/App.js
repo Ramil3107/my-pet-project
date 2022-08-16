8ea028081db849e14dbed26537b7ddc4f1cede99
Ramil3107 <106266508+Ramil3107@users.noreply.github.com>
2022-08-16 23:29:29 +0300

    add create component & json-server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { purple } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { typography } from '@mui/system';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Create from './components/Create';
-import Notes from './components/Notes';
+import Create from './pages/Create';
+import Notes from './pages/Notes';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe"
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Notes />} />
-      <Route path='create' element={<Create  />} />
-    </Routes>
-  );  
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path='/' element={<Notes />} />
+        <Route path='create' element={<Create />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Create.js
@@ -0,0 +1,113 @@
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from "@mui/material"
+import { Container } from "@mui/system"
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import React, { useState } from "react"
+
+
+
+function Create() {
+
+    const classes = {
+        field: {
+            marginBottom: "20px",
+            marginTop: "20px",
+            display: "block",
+        },
+    }
+
+    const [title, setTitle] = useState("")
+    const [details, setDetails] = useState("")
+    const [titleError, setTitleError] = useState(false)
+    const [detailsError, setDetailsError] = useState(false)
+    const [category, setCategory] = useState("todos")
+
+    let submitHandler = (e) => {
+        e.preventDefault()
+
+        setTitleError(false)
+        setDetailsError(false)
+
+        if (!title) {
+            setTitleError(true)
+        }
+        if (!details) {
+            setDetailsError(true)
+        }
+        if (title && details) {
+            console.log(title, details, category)
+            setDetails("")
+            setTitle("")
+        }
+    }
+
+    return (
+
+        <Container>
+
+            <Typography
+                variant="h6"
+                color="textSecondary"
+                component="h2"
+                gutterBottom
+            >
+                Create a New Note
+            </Typography>
+
+            <form
+                onSubmit={submitHandler}
+                noValidate
+                autoComplete="off"
+            >
+
+                <TextField
+                    sx={classes.field}
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    error={titleError}
+                    label="Note Title"
+                    variant="outlined"
+                    color="secondary"
+                    fullWidth
+                    required
+                />
+
+                <TextField
+                    sx={classes.field}
+                    error={detailsError}
+                    value={details}
+                    onChange={(e) => setDetails(e.target.value)}
+                    label="Details"
+                    variant="outlined"
+                    color="secondary"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    required
+                />
+
+                <FormControl sx={classes.field}>
+                    <FormLabel color="secondary">Note Category</FormLabel>
+                    <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+                        <FormControlLabel value="money" control={<Radio color="secondary" />} label="Money" />
+                        <FormControlLabel value="todos" control={<Radio color="secondary" />} label="Todos" />
+                        <FormControlLabel value="reminders" control={<Radio color="secondary" />} label="Reminders" />
+                        <FormControlLabel value="work" control={<Radio color="secondary" />} label="Work" />
+                    </RadioGroup>
+                </FormControl>
+
+                <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    endIcon={<KeyboardArrowRightIcon />}
+                >
+                    Submit
+                </Button>
+
+            </form>
+
+        </Container>
+    )
+}
+
+export default Create
