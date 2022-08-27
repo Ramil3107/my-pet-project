5f552c20b40b542b70b03a9283fb413b0ec01e56
Ramil3107 <abbasov.ram3107@gmail.com>
2022-08-28 02:02:05 +0300

    add skills block

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,24 +1,49 @@
 import { GitHub, LinkedIn, LocationOn } from "@mui/icons-material"
-import { Avatar, Grid, Typography } from "@mui/material"
+import { Avatar, Paper, SvgIcon, Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import resumeAvatar from "../../assets/resumeAvatar.JPG"
-
+import html from "../../assets/iconsPNG/html.png"
+import css from "../../assets/iconsPNG/css.png"
+import js from "../../assets/iconsPNG/js.png"
+import react from "../../assets/iconsPNG/react.png"
+import reactNative from "../../assets/iconsPNG/reactNative.png"
+import form from "../../assets/iconsPNG/form.png"
+import mui from "../../assets/iconsPNG/mui.png"
+import reduxicon from "../../assets/iconsPNG/reduxicon.png"
+import axiosIcon from "../../assets/iconsPNG/axiosIcon.png"
+import routerDom from "../../assets/iconsPNG/routerDom.png"
+import firebaseIcon from "../../assets/iconsPNG/firebaseIcon.png"
+import { Link } from "react-router-dom"
 
 const Resume = () => {
+
+    const styles = {
+        paper: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "max-content",
+            height: 50,
+            float: "left",
+            mt: 3,
+            mr: 3,
+            pl: 1,
+            pr: 1
+        }
+    }
+
     return (
         <Box align="center">
 
+            <Box sx={{ width: "60%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center", mt: 5 }}>
 
 
-
-            <Box sx={{ width: "60%", display: "flex", flexWrap: "wrap", mt: 5 }}>
-
-
-                <Box align="center" sx={{ boxSizing: "border-box", width: "30%" }}>
-                    <Avatar src={resumeAvatar} sx={{ boxSizing: "border-box", width: 120, height: 120 }} />
+                <Box sx={{ boxSizing: "border-box", width: "180px" }}>
+                    <Avatar src={resumeAvatar} sx={{ width: 120, height: 120 }} />
                 </Box>
 
-                <Box sx={{ boxSizing: "border-box", width: "70%" }}>
+                <Box sx={{ boxSizing: "border-box", width: "650px" }}>
+
                     <Typography
                         gutterBottom
                         sx={{ ml: 0, fontWeight: "600" }}
@@ -27,6 +52,7 @@ const Resume = () => {
                     >
                         Ramil Abbasov
                     </Typography>
+
                     <Typography
                         gutterBottom
                         sx={{ ml: 0, fontWeight: "500" }}
@@ -34,24 +60,26 @@ const Resume = () => {
                     >
                         I have 3 years experience as a freelance logo designer. With ideas and analysis of customer data, I can help maximize their branding process.
                     </Typography>
+
                     <Typography
                         gutterBottom
-                        sx={{ ml: 0, fontWeight: "500", display: "flex", alignItems: "center" }}
+                        sx={{ fontWeight: "500", display: "flex", alignItems: "center" }}
                         align="left"
                     >
                         <LocationOn color="secondary" fontSize="medium" />
                         Surabaya City, East Java, Indonesia
-                        <LinkedIn sx={{ ml: 1 }} color="secondary" fontSize="medium" />
+                        <LinkedIn onClick={() => "hi" } sx={{ ml: 1 }} color="secondary" fontSize="medium" />
                         <GitHub sx={{ ml: 1 }} color="secondary" fontSize="medium" />
                     </Typography>
                 </Box>
 
 
 
-                <Box >
+                <Box sx={{ mt: 10, float: "left", width: "100%" }}>
+
                     <Typography
-                        sx={{ fontWeight: 600, mt: 10 }}
-                        variant="h5"
+                        sx={{ fontWeight: 600, }}
+                        variant="h4"
                         color="secondary"
                         align="left"
                     >
@@ -66,6 +94,69 @@ const Resume = () => {
                     >
                         Tech Stack
                     </Typography>
+
+
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={html} />
+                        <Typography sx={{ ml: 1 }}>HTML</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={css} />
+                        <Typography sx={{ ml: 1 }}>CSS</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={js} />
+                        <Typography sx={{ ml: 1 }}>Java Script</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={react} />
+                        <Typography sx={{ ml: 1 }}>React</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 35, height: 35 }} src={reactNative} />
+                        <Typography sx={{ ml: 1 }}>React Native</Typography>
+                    </Paper>
+
+                </Box>
+
+
+                <Box sx={{ mt: 3, float: "left", width: "100%" }}>
+
+                    <Typography
+                        sx={{ mt: 2.5 }}
+                        variant="h6"
+                        color="text.secondary"
+                        align="left"
+                    >
+                        Libraries / Services
+                    </Typography>
+
+
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={mui} />
+                        <Typography sx={{ ml: 1 }}>Material UI</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={reduxicon} />
+                        <Typography sx={{ ml: 1 }}>Redux/React-Redux</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 45, height: 45 }} src={form} />
+                        <Typography sx={{ ml: 1 }}>React Hook Form</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={axiosIcon} />
+                        <Typography sx={{ ml: 1 }}>Axios</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={routerDom} />
+                        <Typography sx={{ ml: 1 }}>React Router Dom(v6)</Typography>
+                    </Paper>
+                    <Paper sx={styles.paper}>
+                        <img style={{ width: 30, height: 30 }} src={firebaseIcon} />
+                        <Typography sx={{ ml: 1 }}>Firebase</Typography>
+                    </Paper>
+
                 </Box>
 
             </Box>
