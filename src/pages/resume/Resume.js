8fbf5c35644fe36bf9aa03894605a1aabbe6129e
Ramil3107 <abbasov.ram3107@gmail.com>
2022-08-27 23:29:30 +0300

    refactor flex

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -8,51 +8,73 @@ const Resume = () => {
     return (
         <Box align="center">
 
-            <Box sx={{ width: "70%", mt:5}}>
-                <Grid container>
-                    <Grid item xs={6} md={3}>
-                        <Avatar src={resumeAvatar} sx={{ width: 130, height: 130 }} />
-                    </Grid>
-                    <Grid item xs={6} md={9} sx={{ml:0}}>
-                        <Typography
-                            gutterBottom
-                            sx={{ ml: 0, fontWeight: "600" }}
-                            align="left"
-                            variant="h5"
-                        >
-                            Ramil Abbasov
-                        </Typography>
-                        <Typography
-                            gutterBottom
-                            sx={{ ml: 0, fontWeight: "500" }}
-                            align="left"
-                        >
-                            I have 3 years experience as a freelance logo designer. With ideas and analysis of customer data, I can help maximize their branding process.
-                        </Typography>
-                        <Typography
-                            gutterBottom
-                            sx={{ ml: 0, fontWeight: "500", display: "flex", alignItems: "center" }}
-                            align="left"
-                        >
-                            <LocationOn color="secondary" fontSize="medium" />
-                            Surabaya City, East Java, Indonesia
-                            <LinkedIn sx={{ ml: 1 }} color="secondary" fontSize="medium" />
-                            <GitHub sx={{ ml: 1 }} color="secondary" fontSize="medium" />
-                        </Typography>
-                    </Grid>
-                </Grid>
+
+
+
+            <Box sx={{ width: "60%", display: "flex", flexWrap: "wrap", mt: 5 }}>
+
+
+                <Box align="center" sx={{ boxSizing: "border-box", width: 200 }}>
+                    <Avatar src={resumeAvatar} sx={{ boxSizing: "border-box", width: 120, height: 120 }} />
+                </Box>
+
+                <Box sx={{ boxSizing: "border-box", width: 600 }}>
+                    <Typography
+                        gutterBottom
+                        sx={{ ml: 0, fontWeight: "600" }}
+                        align="left"
+                        variant="h5"
+                    >
+                        Ramil Abbasov
+                    </Typography>
+                    <Typography
+                        gutterBottom
+                        sx={{ ml: 0, fontWeight: "500" }}
+                        align="left"
+                    >
+                        I have 3 years experience as a freelance logo designer. With ideas and analysis of customer data, I can help maximize their branding process.
+                    </Typography>
+                    <Typography
+                        gutterBottom
+                        sx={{ ml: 0, fontWeight: "500", display: "flex", alignItems: "center" }}
+                        align="left"
+                    >
+                        <LocationOn color="secondary" fontSize="medium" />
+                        Surabaya City, East Java, Indonesia
+                        <LinkedIn sx={{ ml: 1 }} color="secondary" fontSize="medium" />
+                        <GitHub sx={{ ml: 1 }} color="secondary" fontSize="medium" />
+                    </Typography>
+                </Box>
+
+
+
+                <Box >
+                    <Typography
+                        sx={{ fontWeight: 600, mt: 10 }}
+                        variant="h5"
+                        color="secondary"
+                        align="left"
+                    >
+                        Skill
+                    </Typography>
+
+                    <Typography
+                        sx={{ mt: 3 }}
+                        variant="h6"
+                        color="text.secondary"
+                        align="left"
+                    >
+                        Tech Stack
+                    </Typography>
+                </Box>
+
             </Box>
 
-            <Box sx={{ width: "70%", mt:5}}>
-                <Typography
-                sx={{fontWeight:600}}
-                variant="h5"
-                color="secondary"
-                align="left"
-                >
-                    Skill
-                </Typography>
-            </Box>
+
+
+
+
+
         </Box>
     )
 }
