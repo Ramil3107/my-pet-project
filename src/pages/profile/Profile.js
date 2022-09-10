7d2ee7bc0462b42273564e5e642007d358d7236f
Ramil3107 <abbasov.ram3107@gmail.com>
2022-09-11 01:14:15 +0300

    add profile edit mode

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -6,8 +6,9 @@ import { useAuth } from "../../hooks/useAuth"
 import { removeUser, setDisplayName, setPhotoUrl } from "../auth/redux/userSlice"
 import defaultAvatar from "../../assets/defaultAvatar.png"
 import { Box } from "@mui/system"
-import { Avatar, Button, Divider, Grid, Input, Typography } from "@mui/material"
+import { Avatar, Badge, Button, Divider, Grid, IconButton, Input, TextField, Typography } from "@mui/material"
 import { blue } from "@mui/material/colors"
+import { Add, Camera, PhotoCamera, Upload } from "@mui/icons-material"
 
 
 
@@ -55,7 +56,7 @@ const Profile = () => {
                     <Box sx={{ width: "100%", mt: 3, ml: 1, mr: 1, }}>
 
                         <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}>
-                            <Avatar sx={{ width: 150, height: 150 }} src={photoURL} />
+                            <Avatar sx={{ width: 150, height: 150 }} src={photoURL || defaultAvatar} />
                             <Box sx={{ ml: 3 }}>
                                 <Typography variant="h5" >{displayName}</Typography>
                                 <Typography sx={{ mb: 2 }} color={blue[500]} variant="subtitle1">User ID: {id}</Typography>
@@ -119,22 +120,62 @@ const Profile = () => {
                     </Box>
                     :
                     <>
-                        <h1>Welcome {displayName ? displayName : email}</h1>
-                        <button onClick={logoutHandler}>Log Out</button>
-                        <div style={{ marginTop: 50 }}>
-                            <input type="file" onChange={browseFileHandler} />
-                            <button disabled={!photo} onClick={() => uploadPhoto(photo, id)}>Upload Photo</button>
-                            <img
-                                style={{ borderRadius: "100%", width: 50, height: 50 }}
-                                alt="avatar"
-                                src={photoURL ? photoURL : defaultAvatar}
-                            />
-                        </div>
-                        <div style={{ marginTop: 50 }}>
-                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                            <button disabled={!name} onClick={() => uploadName(name)}>Upload Name</button>
-                            <mark style={{ marginLeft: 30 }}>{displayName ? displayName : "Set your name"}</mark>
-                        </div>
+                        <Box sx={{ width: "100%", mt: 3, ml: 1, mr: 1, }}>
+
+                            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}>
+                                <Avatar sx={{ width: 150, height: 150 }} src={photoURL || defaultAvatar} />
+                                <Box sx={{ ml: 3 }}>
+                                    <Typography variant="h5" >{displayName}</Typography>
+                                    <Typography sx={{ mb: 2 }} color={blue[500]} variant="subtitle1">User ID: {id}</Typography>
+                                    <Button
+                                        sx={{ mr: 3 }}
+                                        size="small"
+                                        variant="outlined"
+                                        color="success"
+                                        onClick={() => setEditMode(!editMode)}
+                                    >
+                                        Save
+                                    </Button>
+                                    <Button sx={{ mr: 0.5 }} size="small" variant="outlined" color="info" endIcon={<PhotoCamera />} aria-label="upload picture" component="label">
+                                        Select file
+                                        <input hidden accept="image/*"
+                                            onChange={browseFileHandler} type="file" />
+                                    </Button>
+                                    <Button onClick={() => uploadPhoto(photo, id)} disabled={!photo} size="small" variant="outlined" color="warning" endIcon={<Upload />} aria-label="upload picture" component="label">
+                                        Upload
+                                    </Button>
+                                    <Typography></Typography>
+                                </Box>
+                            </Box>
+
+                            <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+                                <Box sx={{ ml: 0, width: "80%", display: "flex", flexDirection: "column", flexWrap: "wrap" }}>
+                                    <Divider variant="fullwidth" color="white" sx={{ mt: 5, color: "black", display: "block" }} />
+                                    <Box sx={{ display: "flex", flexWrap: "wrap", alignItems: "center", p: 3 }}>
+                                        <Typography sx={{ mr: 1 }} variant="h6">
+                                            Nickname:
+                                        </Typography>
+                                        <TextField
+                                            sx={{ mr: 3 }}
+                                            variant="standard"
+                                            color="secondary"
+                                            value={name}
+                                            onChange={(e) => setName(e.target.value)}
+                                            placeholder={displayName}
+                                        />
+                                        <Button
+                                            variant="text"
+                                            color="warning"
+                                            disabled={!name}
+                                            onClick={() => uploadName(name)}
+                                        >
+                                            Change
+                                        </Button>
+                                    </Box>
+                                    <Divider variant="fullwidth" color="white" sx={{ color: "black" }} />
+                                </Box>
+                            </Box>
+                        </Box>
                     </>
             }
         </>
