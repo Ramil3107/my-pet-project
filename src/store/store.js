8304a2140c79c52d63687561f6e4a5864e37066f
Ramil3107 <abbasov.ram3107@gmail.com>
2022-09-12 00:42:38 +0300

    remove serializable check errors

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userSlice from "../pages/auth/redux/userSlice";
 import enjoySlice from "../pages/enjoy/redux/enjoySlice";
 import homeSlice from "../pages/home/redux/homeSlice";
@@ -10,8 +10,8 @@ export const store = configureStore({
         user: userSlice,
         home: homeSlice,
         enjoy: enjoySlice,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    })
 })
-
-
-window.store = store.getState()
