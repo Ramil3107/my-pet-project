ec5a94a271ed75fa553029551948b94b0b4fad3a
Ramil3107 <abbasov.ram3107@gmail.com>
2022-09-14 18:45:07 +0300

    change broeser router to hash router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import "./firebase";
@@ -10,11 +10,11 @@ import "./firebase";
 
 ReactDOM.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
   // </React.StrictMode>
   ,
   document.getElementById('root')
